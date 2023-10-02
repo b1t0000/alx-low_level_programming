76638623bd9dad7a7f01698872426a23f89a8435
Tewodros <tadane27@gmail.com>
2023-10-02 12:03:05 -0700

    prints the progra name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: number of argument
+ * @argv: array of arguments
+ * Return: 0 succes
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
