cdf52fb9f35d251c217fbf3574cc667bdc4a1825
Tewodros <tadane27@gmail.com>
2023-10-02 12:10:41 -0700

    number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: number of argument
+ * @argv: array of arguments
+ * Return: 0 succes
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
