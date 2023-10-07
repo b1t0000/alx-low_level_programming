807c2faabf9ccf9118a1685d7be08c80c24a87a1
Tewodros <tadane27@gmail.com>
2023-10-07 16:48:14 -0700

    concatenate

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * string_nconcat - concatenates tow strings
+ * @s1: string one
+ * @s2: string two
+ * @n: string 2 number of word two concatenate
+ * Return: pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ss;
+	unsigned int i = 0, j = 0, k, t;
+
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	k = i + n + 1;
+	ss = malloc(k);
+	if (!ss)
+		return (NULL);
+	if (j < n)
+		n = j;
+	for (t = 0; t < i; t++)
+	{
+		ss[t] = s1[t];
+	}
+	for (i = 0; i < n; i++)
+	{
+		ss[t] = s2[i];
+		t++;
+	}
+	return (ss);
+}
