e516bfd08511cff2de95aeb6840dd295af04bd92
Tewodros <tadane27@gmail.com>
2023-10-07 17:29:27 -0700

    call

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *cal;
 	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	cal = malloc(nmemb * size);
 	if (!cal)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		cal[i] = 0;
+	for (i = 0; i < nmemb * size; i++)
+		cal[i] ='0';
 	return (cal);
 }
