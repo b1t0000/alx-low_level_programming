21957cb48fc29323741e771e07d462e53200cfa4
Tewodros <tadane27@gmail.com>
2023-10-07 17:24:15 -0700

    call

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _calloc - calloc using malloc
+ * @nmemb: meebers number
+ * @size: size of one member
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *cal;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+	cal = malloc(nmemb * size);
+	if (!cal)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		cal[i] = 0;
+	return (cal);
+}
