58f63c10f7da265f84c1089893723b0423048674
Tewodros <tadane27@gmail.com>
2023-10-09 16:47:56 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,59 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * _copy - copy
+ * @pst: pest
+ * @cp: copeid
+ * Return: the pointer
+ */
+char *_copy(char *pst, char *cp)
+{
+	int i = 0, j;
+
+	while (cp[i] != '\0')
+		i++;
+	for (j = 0; j < i; j++)
+	{
+		pst[j] = cp[j];
+	}
+	pst[j] = '\0';
+	return (pst);
+}
+
+/**
+ * new_dog - new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: pointer
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	int i = 0, j = 0;
+
+	new = malloc(sizeof(dog_t));
+	if (!new)
+		return (NULL);
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	new->name = malloc(i);
+	if (new->name == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->age = age;
+	new->owner = malloc(j);
+	if (new->owner == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	_copy(new->name, name);
+	_copy(new->owner, owner);
+	new->age = age;
+	return (new);
+}
