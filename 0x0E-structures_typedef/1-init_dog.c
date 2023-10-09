fade75d1b19ca861456c47b1c8776b7162d90822
Tewodros <tadane27@gmail.com>
2023-10-09 15:32:34 -0700

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,7 +3,7 @@
  * init_dog - initialize struct element
  * @d: pointer to struct
  * @name: name of dog
- * @aeg: age of dog
+ * @age: age of dog
  * @owner: owner of dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
