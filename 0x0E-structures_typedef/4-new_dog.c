0c93787355857bbd3af62fff83eac6ca481f2bcb
Tewodros <tadane27@gmail.com>
2023-10-09 17:00:46 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -41,11 +41,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new->name = malloc(i + 1);
 	if (new->name == NULL)
 	{
+		free(new->name);
+		free(new);
 		return (NULL);
 	}
 	new->owner = malloc(j + 1);
 	if (new->owner == NULL)
 	{
+		free(new->owner);
+		free(new);
 		return (NULL);
 	}
 	_copy(new->name, name);
