0da6396932647b0b9cfa07250a3a5c3258418018
Tewodros <tadane27@gmail.com>
2023-10-11 14:27:33 -0700

    array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - array iterator
+ * @array: array
+ * @size: size of array
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
