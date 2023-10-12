205595966eb29dee1857cf9501f44a23a4880db6
Tewodros <tadane27@gmail.com>
2023-10-12 15:48:53 -0700

    print num

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,7 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if ( n == 0)
+	if (n == 0)
 		return;
 	if (separator == NULL)
 		separator = "";
